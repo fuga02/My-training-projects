8f58b049edeb5c06d761ecf680f70046aff88753
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-06-08 03:03:37 -0700

    Ok

---
+++ PostgreSql Training/PostgreSql Training/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PostgreSql_Training.Context;
+
+public class AppDbContext:DbContext
+{
+    public DbSet<User> Users => Set<User>();
+   // public DbSet<UserNames> UserNames => Set<UserNames>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(
+            "Server=localhost;Port=5432;Database=kerak;User Id=postgres;Password=postgres;");
+    }
+
+    /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserNames>().ToView("user_names");
+    }*/
+}
---
+++ PostgreSql Training/PostgreSql Training/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PostgreSql_Training;
+[Table("users")]
+public class User
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    public required string Name { get; set; }
+
+    [Column("is_admin")]
+    public bool IsAdmin { get; set; }
+
+    [Column("age")]
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, Name: {Name}, IsAdmin: {IsAdmin}, Age: {Age}";
+    }
+}
